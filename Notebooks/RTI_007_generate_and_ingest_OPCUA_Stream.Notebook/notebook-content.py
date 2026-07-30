04ca734d6cac4a68706a125f1260c073807d4674
DibakarMSFT <dibakardharchoudhury@yahoo.co.in>
2026-07-30 18:43:13 +0000

    Committing 9 items from workspace a79a4b7e-e508-4fa4-8b6f-15deadca0f34

--- Notebooks/RTI_007_generate_and_ingest_OPCUA_Stream.Notebook/notebook-content.py
+++ Notebooks/RTI_007_generate_and_ingest_OPCUA_Stream.Notebook/notebook-content.py
@@ -8,12 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "1915afbd-36aa-40f9-bd7e-18a03c4a28b2",
+# META       "default_lakehouse": "c42060fb-abbc-4fa1-ac87-ed0a5c460aaf",
 # META       "default_lakehouse_name": "Energy_IQ_LakehouseRTI_V6",
 # META       "default_lakehouse_workspace_id": "a79a4b7e-e508-4fa4-8b6f-15deadca0f34",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "1915afbd-36aa-40f9-bd7e-18a03c4a28b2"
+# META           "id": "c42060fb-abbc-4fa1-ac87-ed0a5c460aaf"
 # META         }
 # META       ]
 # META     },
